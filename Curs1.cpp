ed76138d9d27966426524bef5bcbd6ea5d62be32
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 22:13:07 +0300

    Add project files.

---
+++ Curs1.cpp
@@ -0,0 +1,30 @@
+// Curs1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// acesta este un comment single line
+/*  acesta este un comment multi line */
+
+
+//includem iostream ca sa folosim std::cout
+#include <iostream>
+
+//using namespace std; namespacesul de std(nume de familie)
+
+
+int main()
+{
+    //se printeaza numele utilizatorului
+    std::cout << "Hello Voicu! \n";
+    std::cout << "Azi e marti" << std::endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
